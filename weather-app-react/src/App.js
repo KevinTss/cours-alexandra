e0f190adcc91e0ff606ba969fb79fdcf580c10b3
KevinTss <kevin.tassi@gmail.com>
2021-04-27 18:37:07 +0200

    Setup basic react app

---
+++ weather-app-react/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <input type="text" placeholder="city" />
+        <button>Search</button>
+      </form>
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
